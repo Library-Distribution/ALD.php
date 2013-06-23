49505e9a63764cc26d7683165e4173ef0a2d5fca
maul.esel <maul.esel@go4more.de>
2013-06-23 18:52:20 +0200

    Add some custom exception classes

---
+++ lib/exceptions.php
@@ -0,0 +1,12 @@
+<?php
+class InvalidXmlException extends Exception {
+}
+class FileNotFoundException extends Exception {
+}
+class ArchiveOpenException extends Exception {
+}
+class UndefinedFileException extends Exception {
+}
+class UnknownSchemaException extends Exception {
+}
+?>
